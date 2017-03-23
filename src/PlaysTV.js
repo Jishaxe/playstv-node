4f4b03499d524ee84445ceed3517578944e6c8db
Josh Lee <josh@joshbe.me>
2017-03-23 20:38:26 +0000

    get user

--- src/PlaysTV.js
+++ src/PlaysTV.js
@@ -1,27 +1,50 @@
 request = require("request")
-const endpoint = "https://api.plays.tv/data/v1"
+querystring = require("querystring")
+const url = "https://api.plays.tv/data/v1"
 
 class PlaysTV {
   constructor(appid, appkey) {
     this.appid = appid
     this.appkey = appkey
+    this.api = this
+
+    this.users = {
+      get: this.getUser.bind(this)
+    }
+  }
+  
+  /**
+   * Makes a GET request to PlaysTV API
+   * @param {String} endpoint The endpoint to use, such as /auth/verify
+   * @param {String} args query params to use, this will add in the app id and key for you
+   */
+  _get(endpoint, args) {
+    args.appid = this.appid
+    args.appkey = this.appkey
+    let query = querystring.stringify(args) 
+
+    return new Promise((resolve, reject) => {
+      request(`${url}${endpoint}?${query}`, (error, response, body) => {
+          if (error) reject(error)
+          else if (response.statusCode !== 200) reject(`Got a ${response.statusCode} instead of 200 on ${endpoint}: ${body}`)
+          else resolve(response)
+      })
+    })
   }
 
   /**
    * Verifies the API key and token given in the constructor
    */
   verify() {
-    let appid = this.appid
-    let appkey = this.appkey
+    return this._get("/auth/verify", {})
+  }
 
-    return new Promise((resolve, reject) => {
-      console.log(`/auth/verify?appid=${appid}&appkey=${appkey}`)
-      request(endpoint + `/auth/verify?appid=${appid}&appkey=${appkey}`, (error, response, body) => {
-          if (error) reject(error)
-          else if (response.statusCode !== 200) reject(`Got a ${response.statusCode} instead of 200 on /auth/verify: ${body}`)
-          else resolve(response)
-      })
-    })
+  /**
+   * Gets a user with the specified username
+   * @param {String} username The username to request
+   */
+  getUser(username) {
+    return this._get(`/users/${username}`, {}).then((response) => Promise.resolve(JSON.parse(response.body).content, (error) => Promise.reject(error)))
   }
 }
 
